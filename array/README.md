f555a50a35ca47378e568995bf0cc32deb3c0c99
Minhang Xie <minhangxie97@gmail.com>
2022-07-04 16:28:49 -0700

    two sum complete

---
+++ array/README.md
@@ -0,0 +1,10 @@
+- [X] Two Sum
+- [ ] Best Time to Buy and Sell Stock
+- [ ] Contains Duplicate
+- [ ] Product of Array Except Self
+- [ ] Maximum Subarray
+- [ ] Maximum Product Subarray
+- [ ] Find Minimum in Rotated Sorted Array
+- [ ] Search in Rotated Sorted Array
+- [ ] Three Sum
+- [ ] Container With Most Water
---
+++ array/two_sum.py
@@ -0,0 +1,11 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range(len(nums)):
+            remainder = target - nums[i]
+            if remainder in dict:
+                return [dict.get(target-nums[i]), i],
+            else:
+                dict[nums[i]] = i
